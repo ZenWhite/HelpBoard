d7cc1ef4b1ad65793fd05eacf1cb0f96b84c0bb7
YI <yurkimus@gmail.com>
2021-02-13 23:12:43 +0300

    server: clowning around

--- server/src/index.js
+++ server/src/index.js
@@ -11,6 +11,10 @@ app.use(express.json({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
 app.use((request, response, next) => {
   response.header('Access-Control-Allow-Origin', '*')
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
   next()
 })
 
