8494d5c2dc96dc6f268e646b0f08f2f15cc1db12
YI <yurkimus@gmail.com>
2021-02-15 12:42:29 +0300

    implement crud for tasklist entity

--- server/src/modules/tasklist/tasklist.router.js
+++ server/src/modules/tasklist/tasklist.router.js
@@ -1,22 +1,50 @@
 import express from 'express'
+import feedback from '../../misc/feedback.js'
+import { tasklistService } from './tasklist.service.js'
 
 const router = express.Router()
 
-const routes = {
-  base: '/',
-  create: '/create',
-  update: '/update'
-}
-
 router
-  .get(routes.base, (request, response) => {
-    response.json({ message: 'read tasklist' })
+  .get('/', async (request, response) => {
+    const tasklists = await tasklistService.readAll()
+
+    tasklists
+      ? response.json({ tasklists })
+      : response.send(feedback.empty('tasklists'))
   })
-  .post(routes.create, (request, response) => {
-    response.json({ message: 'create tasklist' })
+  .get('/:id', async (request, response) => {
+    const { id } = request.params
+
+    const tasklist = await tasklistService.readOne(id)
+
+    tasklist
+      ? response.json({ tasklist })
+      : response.send(feedback.empty(`tasklist with id ${id}`))
   })
-  .put(routes.update, (request, response) => {
-    response.json({ message: 'update tasklist' })
+  .post('/create', async (request, response) => {
+    const tasklist = await tasklistService.createOne(request.body)
+
+    tasklist
+      ? response.json({ tasklist })
+      : response.send(feedback.notCreated('tasklist'))
+  })
+  .put('/:id/update', async (request, response) => {
+    const { id } = request.params
+
+    const tasklist = await tasklistService.updateOne(id, request.body)
+
+    tasklist
+      ? response.json({ tasklist })
+      : response.send(feedback.notUpdated(`tasklist with id ${id}`))
+  })
+  .delete('/:id/delete', async (request, response) => {
+    const { id } = request.params
+
+    const tasklist = await tasklistService.deleteOne(id)
+
+    tasklist
+      ? response.json({ tasklist })
+      : response.send(feedback.notDeleted(`tasklist with id ${id}`))
   })
 
 export { router }
