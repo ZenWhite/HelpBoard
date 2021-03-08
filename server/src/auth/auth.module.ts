1d430de59ea463ba6bef012363bb5776976c9833
YI <yurkimus@gmail.com>
2021-03-08 18:13:15 +0300

    return token on valid user

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { UserRepository } from 'src/database/entities/user/user.repository'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt'
+    }),
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: {
+        expiresIn: 3600
+      }
+    }),
+    TypeOrmModule.forFeature([UserRepository])
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,11 +1,19 @@
-import { Injectable } from '@nestjs/common'
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException
+} from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 import { SignInDTO } from 'src/database/dto/auth.dto'
 import { UserRepository } from 'src/database/entities/user/user.repository'
 import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService
+  ) {}
 
   private async validatePassword({ email, password }: SignInDTO) {
     const found = await this.userRepository.findOne({ email })
@@ -14,12 +22,18 @@ export class AuthService {
       const hash = await bcrypt.hash(password, found.salt)
 
       return hash === found.password
-    } else return false
+    } else throw new NotFoundException(`Cannot get user with email: ${email}`)
   }
 
   public async signIn(data: SignInDTO) {
     const isPasswordValid = await this.validatePassword(data)
 
-    console.log(isPasswordValid)
+    if (isPasswordValid) {
+      const payload = { email: data.email }
+
+      const accessToken = await this.jwtService.sign(payload)
+
+      return { accessToken }
+    } else throw new UnauthorizedException(`Cannot login user`)
   }
 }
